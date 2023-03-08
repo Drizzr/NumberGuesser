e530f4646087b36bb8ce0b07f6ae8e65d213b37b
Drizzr <samuelfriese@gmx.net>
2023-03-08 19:08:08 +0100

    Implementaion of Sctivation Functions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-X = np.array([[1, 2, 3, 2.5],
-                [2.0, 5.0,-1.0, 2.0],
-                [-1.5, 2.7, 3.3, -0.8]]
-                )
+nnfs.init()
 
 class Layer_Dense:
 
@@ -19,10 +18,25 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
 
-layer1.forward(X)
-layer2.forward(layer1.output)
-print(layer2.output)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs-np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
+
+X,y = spiral_data(100, 3)
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
